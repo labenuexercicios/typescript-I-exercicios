c8a6a2aff0873539b837ae4bf03c184d824cd65b
unknown <guilhermer_mesquita@hotmail.com>
2023-01-15 01:41:32 -0400

    Mockando mais usuários

--- src/database.ts
+++ src/database.ts
@@ -4,10 +4,30 @@ export const users: DUser[] = [{
     id: '1',
     email: 'string@gmail',
     password: '123'
+},
+{
+    id: '2',
+    email: 'email@email',
+    password: '123'
+},
+{
+    id: '3',
+    email: 'myemail@hotmail',
+    password: '123'
 }]
 
 export const products: DProduct[] = [{
     id: '1',
+    name: 'Iphone12',
+    price: 8500,
+    category: Categoria.ELECTRONICS
+},{
+    id: '2',
+    name: 'Iphone12',
+    price: 8500,
+    category: Categoria.ELECTRONICS
+},{
+    id: '3',
     name: 'Iphone12',
     price: 8500,
     category: Categoria.ELECTRONICS
@@ -22,6 +42,17 @@ for (let i in products){
 export const purchase: DPurchase[] = [{
     userId: users[0].id,
     productId: products[0].id,
+    quantity: products.length,
+    totalPrice: quantity
+},
+{
+    userId: users[1].id,
+    productId: products[1].id,
+    quantity: products.length,
+    totalPrice: quantity
+},{
+    userId: users[2].id,
+    productId: products[1].id,
     quantity: products.length,
     totalPrice: quantity
 }]
