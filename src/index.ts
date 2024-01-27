bb46fa7a1672e06d3931697f35f9cfff7fa5e8d1
Giovanni Souza <GSMartins999>
2024-01-27 18:00:31 -0300

    exertype

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { users, products } from "./database";
+
+console.table(users)
+console.log("\n")
+console.table(products)
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type TUsers = {
+    id: string,
+    name:string,
+    email: string,
+    password: string,
+    createdAt: string //no formato ano-mês-dia T hora:minuto:segundo:milésimo-de-segundos Z
+}
+
+export type TProducts = {
+    id: string,
+    name:string,
+    price: number,
+    description: string,
+    imageUrl: string
+}
+
+
